b3814c7d8cf2a3fe6bbe7be0236b22871de18a27
hizaalzz <hizaaliza707@gmail.com>
2022-08-16 13:20:41 +0700

    daftarsoal

--- resources/views/components/test-sidebar.blade.php
+++ resources/views/components/test-sidebar.blade.php
@@ -1,12 +1,12 @@
 <div class="sidebar absolute flex flex-col px-3 py-2 right-0 bg-white shadow-lg">
     <div class="flex pb-4 pt-2">
-        {{-- <div class="flex flex-col">
+        <div class="flex flex-col">
             <h3 class="text-xl">Daftar Soal</h3>
             <div class="text-gray-700 text-xs" id="count">
                 <span class="text-blue-500" id="answered">0</span> Terjawab,
                 <span class="text-yellow-700" id="doubt">0</span> Ragu-ragu,
             </div>
-        </div> --}}
+        </div>
         <button class="ml-auto px-2 py-2 bg-transparent hover:bg-gray-100 focus:outline-none closebtn">
             <i class="fas fa-times text-red-500"></i>
         </button>
